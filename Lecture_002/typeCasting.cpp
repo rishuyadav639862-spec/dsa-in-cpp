b620c7ad3711bd980b2dc1f34d4013d8a2582b56
Rishu <rishuyadav639862@gmail.com>
2026-03-16 21:23:38 +0530

    lecture_004

---
+++ Lecture_002/typeCasting.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char grade = 'A';
+
+    int value = grade;
+    cout << value << endl;
+    return 0;
+}
---
+++ Lecture_004/Pattern5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for(int i=n; i>0; i--) {
+        cout << i << endl;
+    }
+    
+    return 0;
+}
