d5554d5ad29576cdbd58698d3a97d236f02a4a0f
Rishu <rishuyadav639862@gmail.com>
2026-03-20 20:16:43 +0530

    lecture_006

---
+++ Lecture_006/binaryToDecimal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int decToBinary(int decNum) {
+    int ans = 0, pow = 1;
+
+    while(decNum > 0) {
+        int rem = decNum % 2;
+        decNum /= 2;
+
+        ans += (rem * pow);
+        pow *= 10;
+    }
+
+    return ans; //binary form
+}
+
+int binToDecimal(int binNum) {
+    int ans = 0, pow = 1;
+
+    while(binNum > 0) {
+        int rem = binNum % 10;
+        ans += rem * pow;
+
+        binNum /= 10;
+        pow *= 2;
+    }
+
+    return ans; //decimal form
+}
+
+int main() {
+    cout << binToDecimal(101) << endl; //5
+    return 0;
+}
