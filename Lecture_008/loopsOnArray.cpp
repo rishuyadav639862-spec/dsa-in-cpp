d2368572bde4d1834068b3c124e912d3396c74fa
Rishu <rishuyadav639862@gmail.com>
2026-03-22 22:41:37 +0530

    lecture_008

---
+++ Lecture_008/loopsOnArray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {99, 100, 54, 36, 88};
+    int size = 5;
+
+    //int sz = sizeof(marks) /
+    cout << sizeof(marks) << endl;
+    //loops : 0 to size-1
+    
+    return 0;
+}
